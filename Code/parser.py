4b5a4785a15b0ca0f7c836200b351e0941742693
jonasrothfuss <jonas.rothfuss@gmx.de>
2016-08-18 10:44:16 +0200

    Extra processing of JSON parse trees, if parsing fails in strict mode

--- Code/parser.py
+++ Code/parser.py
@@ -1,5 +1,7 @@
 from pycorenlp import StanfordCoreNLP
 import nltk
+import json
+import xml.etree.cElementTree as ElementTree
 
 class Parser:
     def __init__(self, coreNLPServer ='http://localhost:9000'):
@@ -10,12 +12,15 @@ class Parser:
             'annotators': 'tokenize,ssplit,parse',
             'outputFormat': 'json'
         })
+        if type(nlp_output) == str:
+            nlp_output = json.loads(nlp_output, strict=False)
+
         parse_tree_array = []
         for s in nlp_output['sentences']:
             parse_tree_array.append(nltk.tree.Tree.fromstring(s['parse']))
         return parse_tree_array
 
-    def draw_parse_tree(parse_tree):
+    def draw_parse_tree(self, parse_tree):
         if isinstance(parse_tree, list):
             for t in parse_tree:
                 nltk.draw.tree.draw_trees(t)
