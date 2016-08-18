91c65166b40e7cd2fa0a1963f2253f684fbcdf29
jonasrothfuss <jonas.rothfuss@gmx.de>
2016-08-18 16:59:52 +0200

    conversion of timestamps

--- Code/db_handling.py
+++ Code/db_handling.py
@@ -2,8 +2,14 @@ from pymongo import MongoClient
 import numpy as np
 import time
 import datetime
+import os
+import bson
 from tweet import Tweet
+from parser import Parser
+from sentiment import sentiment
 from tweets_statistic import Tweets_Statistic
+import pickle
+from pprint import pprint
 
 
 def print_dict(dict):
@@ -71,6 +77,7 @@ def round_to_next_minute(dt):
 
 def percentage_tweets_with_in_time_stock_price(tweets_collection,prices_collection, number_of_tweets):
     s = tweets_collection.find()
+    s.batch_size(2000)
     count = 0
     success_count = 0
 
@@ -91,6 +98,58 @@ def percentage_tweets_with_in_time_stock_price(tweets_collection,prices_collecti
 
     return success_count/float(count)
 
+def write_tweets_to_file(tweets_collection, filepath, force=False, limit = 50000000):
+    cursor = tweets_collection.find()
+    cursor.batch_size(1000)
+    count = 0
+    if os.path.exists(filepath) and not force:
+        # override by setting force=True.
+        print('File already present - Skipping pickling.')
+    else:
+        try:
+            with open(filepath, 'wb') as f:
+                for t in cursor:
+                    if count > limit:
+                        break
+                    count += 1
+                    pickle.dump(Tweet(t), f, pickle.HIGHEST_PROTOCOL)
+        except Exception as e:
+            print('Unable to save data', ':', e)
+
+def load_tweets_from_file(file_path):
+    tweet_array = []
+    with open(file_path, 'rb') as f:
+        for _ in range(pickle.load(f)):
+            tweet_array.append(pickle.load(f))
+    return tweet_array
+
+def convert_db_timestamps_to_int(collection, as_bulk = False):
+    cursor = collection.find()
+    count = cursor.count()
+    n = 0
+    if not as_bulk:
+        for tweet in cursor:
+            if n % 1000 == 0:
+                print(n)
+            id = tweet["id"]
+            timestamp = int(tweet["timestamp_ms"])
+            collection.update_one({"id": id}, {'$set': {'timestamp_ms': timestamp}})
+            n += 1
+    else:
+        if n % 1000 == 0:
+            bulk = collection.initialize_ordered_bulk_op()
+        id = tweet["id"]
+        timestamp = int(tweet["timestamp_ms"])
+        bulk.find({"id": id}).update({'$set': {'timestamp_ms': timestamp}})
+        if n == (count-1) or (n % 10 == 9):
+            print("Execute Bulk Operation")
+            result = bulk.execute()
+            pprint(result)
+            print("----> N = " + str(n))
+        n += 1
+
+def tweet_query(collection, start_date, end_date, symbol):
+    return collection.find({"timestamp_ms": {'$gte': start_date, '$lt': end_date}})
 
 if __name__ == '__main__':
     # connect to db_collection
@@ -98,9 +157,7 @@ if __name__ == '__main__':
     tweets_db_collection = get_tweets_collection(db)
     prices_collection = get_prices_collection(db)
 
-    print(percentage_tweets_with_in_time_stock_price(tweets_db_collection, prices_collection, 200000))
-
-
+    convert_db_timestamps_to_int(tweets_db_collection)
 
     '''
     t1 = tweets_db_collection.find()[1]
