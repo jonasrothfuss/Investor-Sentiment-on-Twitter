821be9160eb25e596adea22a31944a2cfcb7fc00
jonasrothfuss <jonas.rothfuss@gmx.de>
2016-08-21 18:20:41 +0200

    sentiment analysis block

--- Code/sentiment.py
+++ Code/sentiment.py
@@ -1,8 +1,15 @@
 from parser import Parser
 import nltk
+import time
 
 p = Parser()
-
+label_score_dict = {
+    "positive" : 1,
+    "neutral" : 0,
+    "negative" : -1,
+    "verypositive" : 2,
+    "verynegative" : -2
+}
 
 def sentiment(text):
     nlp_output = p.nlp.annotate(text, properties={
@@ -10,3 +17,24 @@ def sentiment(text):
         'outputFormat': 'json'
     })
     return nlp_output['sentences'][0]['sentiment']
+
+def sentiment_label_to_score(label):
+    return label_score_dict[label.lower()]
+
+def bulk_sentiment(tweets):
+    cumulated_score = 0
+    sucess_counter = 0
+    ts = time.clock()
+    for t in tweets:
+        try:
+            score = sentiment_label_to_score(sentiment(t.text))
+            cumulated_score += score
+            sucess_counter += 1
+            #print("--- (" + str(score) + ") --- " + t.text)
+            if sucess_counter % 200 == 0:
+                duration = time.clock() - ts
+                ts = time.clock()
+                print("Successfully processed 200 tweets:" + str(sucess_counter) + "  processing time: " + str(duration))
+        except:
+            print("Sentiment analysis failed on: " + t.text)
+    return cumulated_score/float(sucess_counter)
