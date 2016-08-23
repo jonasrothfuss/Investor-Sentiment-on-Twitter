a2e5a6cf2af144ac9307849241cc50b3429b2c58
jonasrothfuss <jonas.rothfuss@gmx.de>
2016-08-23 11:41:21 +0200

    binary parse trees

--- Code/parser.py
+++ Code/parser.py
@@ -1,24 +1,31 @@
 from pycorenlp import StanfordCoreNLP
 import nltk
 import json
-import xml.etree.cElementTree as ElementTree
 
 class Parser:
     def __init__(self, coreNLPServer ='http://localhost:9000'):
         self.nlp = StanfordCoreNLP('http://localhost:9000')
 
-    def parse_tree(self, text):
+    def parse_tree(self, text, binary = False):
         nlp_output = self.nlp.annotate(text, properties={
-            'annotators': 'tokenize,ssplit,parse',
-            'outputFormat': 'json'
+            'annotators': 'tokenize,ssplit,pos,parse',
+            'outputFormat': 'json',
+            'parse.binaryTrees': 'true'
         })
         if type(nlp_output) == str:
             nlp_output = json.loads(nlp_output, strict=False)
 
         parse_tree_array = []
         for s in nlp_output['sentences']:
-            parse_tree_array.append(nltk.tree.Tree.fromstring(s['parse']))
+            p_tree = nltk.tree.Tree.fromstring(s['parse'])
+
+            if binary:
+                nltk.treetransforms.chomsky_normal_form(p_tree)
+
+            parse_tree_array.append(p_tree)
+
         return parse_tree_array
+
 
     def draw_parse_tree(self, parse_tree):
         if isinstance(parse_tree, list):
